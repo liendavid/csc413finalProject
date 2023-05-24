dc04dd39bf808154af016dde2891f03f8b25d4d0
liendavid <dlien@mail.sfsu.edu>
2023-05-24 01:43:42 -0700

    Made status part of navbar and make it a bubble

--- client/src/structure/Navbar.js
+++ client/src/structure/Navbar.js
@@ -15,6 +15,7 @@ const Navbar = () =>  {
     }
 
     let  [username, setUsername] = useState();
+    let  [isUserOnline, setIsUserOnline] = useState(true);
 
     useEffect(() => {
          getLoggedInUsername().then((result) =>  {
@@ -25,6 +26,31 @@ const Navbar = () =>  {
 
     }, []);
 
+    useEffect(() => {
+        const checkUserOnlineStatus = async () => {
+          try {
+            const body = {
+                userName: username,
+            };
+            const httpSettings = {
+                body: JSON.stringify(body),
+                method: 'POST'
+            };
+            const response = await fetch('/statusCheck', httpSettings);
+            const data = await response.json();
+            setIsUserOnline(data.status);
+          } catch (error) {
+            setIsUserOnline(false);
+          }
+        };
+    
+        checkUserOnlineStatus();
+        const intervalId = setInterval(checkUserOnlineStatus, 5000);
+    
+        return () => {
+          clearInterval(intervalId);
+        };
+      }, []);
 
 
     return (
@@ -50,6 +76,23 @@ const Navbar = () =>  {
                 : ''}
 
             {isAuth() ? <a onClick={logoutAction}>Logout ({ username })</a> : ""}
+
+            {isAuth() ?
+                <div className="dropdown">
+                    <button className="dropdown-button">
+                    <span className={`status-indicator ${isUserOnline ? 'online' : 'offline'}`} />
+                    <i className="fa fa-caret-down" aria-hidden="true"/>
+                    </button>
+                    <div className="dropdown-content">
+                        <a href="#" onClick={() => {goToPage("/friend-list")}}>Online</a>
+                        <a href="#" onClick={() => {goToPage("/send-friend-request")}}>Away</a>
+                        <a href="#" onClick={() => {goToPage("/view-friend-requests")}}>Invisible</a>
+                    </div>
+                </div>
+                : null}
+            {/* <div className={`status-indicator ${isUserOnline ? 'online' : 'offline'}`}>
+                {isUserOnline ? 'Online' : 'Offline'}
+            </div> */}
         </div>
     );
 };
