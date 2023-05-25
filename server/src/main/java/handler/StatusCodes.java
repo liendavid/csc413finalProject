d9c43c69efcbf0bf76e7286b6c18e57539a564c5
liendavid <dlien@mail.sfsu.edu>
2023-05-24 21:38:44 -0700

    status is now stored in the DB

--- server/src/main/java/handler/StatusCodes.java
+++ server/src/main/java/handler/StatusCodes.java
@@ -5,5 +5,6 @@ public class StatusCodes {
   public static final String UNAUTHORIZED = "401 Unauthorized";
   public static final String OK = "200 OK";
   public static final String SERVER_ERROR = "500 Internal Server Error";
+  public static final String BAD_REQUEST = "400 Bad Request";
 
 }
